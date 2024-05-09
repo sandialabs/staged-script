b23355399007e17f023750d33a46020b33b817fb
Jason M. Gates <jmgate@sandia.gov>
2024-05-09 17:32:01 -0600

    refactor: Reduce setup file
    Remove the guts of the `setup.py` file so it just uses the
    `pyproject.toml` under the hood.

--- setup.py
+++ setup.py
@@ -4,18 +4,7 @@ Setup file for the ``staged-script`` package.
 To install, simply ``python3 -m pip install .`` in the repository root.
 """
 
-from setuptools import setup
+import setuptools
 
-setup(
-    name="staged-script",
-    version="1.0.0",
-    description=(
-        "A base class to inherit from when building scripts that are "
-        "subdivided into a series of stages."
-    ),
-    packages=["staged_script"],
-    scripts=[],
-    python_requires=">=3.10",
-    tests_require=["pytest==7.1.1"],
-    install_requires=["rich==12.5.1"],
-)
+if __name__ == "__main__":
+    setuptools.setup()
